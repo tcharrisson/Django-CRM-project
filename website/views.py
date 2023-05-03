574ad811696337a5874215b58f1574885cf141a9
Hilary Ifezue <tcharrisson@gmail.com>
2023-05-03 08:32:34 +0100

    first frontend changes commit

--- website/views.py
+++ website/views.py
@@ -1,9 +1,29 @@
 '''
 working with renders
 '''
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
-    # creating home requests
-    return render(request, 'home.html', {})
+    '''check to see if logging in
+    '''
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        #authenticate
+        user = authenticate(request, username =username, password =password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, message= 'you have been logged in!')
+            return redirect("home")
+        else:
+            messages.success(request, message= "There was an error while logging in, Try again ")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
+def logout_user(request):
+    '''logoout function
+    '''
+    pass

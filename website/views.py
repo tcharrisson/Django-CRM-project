bf4d9f08b998bd6527b7784c7e354a0935b8e64e
Hilary Ifezue <tcharrisson@gmail.com>
2023-05-04 10:37:26 +0100

    sorted the html tags after the display was altered

--- website/views.py
+++ website/views.py
@@ -4,6 +4,8 @@ working with renders
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
+
 
 def home(request):
     '''check to see if logging in
@@ -33,4 +35,18 @@ def logout_user(request):
 def register_user(request):
     '''users registration function
     '''
-    return render(request, 'register.html', {})
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You have been successfully Registered. welcome!...")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
+    return render(request, 'register.html', {'form':form})
